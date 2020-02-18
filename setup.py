b023ad650339aa791398bbba81ffc8b426cde44d
Ildar Lomov <ildar.lomov@ostrovok.ru>
2020-02-18 11:41:45 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Making a model for Tennessee Eastman Process data',
+    author='ildar.lomov@yandex.ru',
+    license='MIT',
+)
